f5c1e84cf4aae126b0e6e62f5bf7b5daca06d923
Chloewaiwai <waiyip814@gmail.com>
2023-05-12 01:33:01 +0800

    add progress bar in skills

--- src/components/About.js
+++ src/components/About.js
@@ -6,19 +6,19 @@ export default function About() {
     <div >
       <img src={process.env.PUBLIC_URL+'/about-background.png'} className='about-background' alt="coloured background" />
       <div className='container padding'>
-      <div className='row'>
-        <div className='col-7'>
-            <h2>Here's Chloe Yip!</h2>
-            <h4>Bachelor’s degree of</h4>
-            <h1>Fintech & AI</h1>
-            <p>I love developing <b>website</b>, building <b>AI models</b>. <b>3D modelling</b> is also my favourite! I am looking for more <b>job opportunities</b> to work in coding and modelling on both <b>regular</b> and <b>freelance</b> basis.</p>
-            <p className='leading-text'>Check out what've I built and made below !</p>
-          </div >
-        <div className='col-5'>
-          <img src={process.env.PUBLIC_URL+'/me.png'} className='myphoto rounded-circle' alt="chloe's icon"/>
-        </div>
+        <div className='row'>
+          <div className='col-7'>
+              <h2>Here's Chloe Yip!</h2>
+              <h4>Bachelor’s degree of</h4>
+              <h1>Fintech & AI</h1>
+              <p>I love developing <b>website</b>, building <b>AI models</b>. <b>3D modelling</b> is also my favourite! I am looking for more <b>job opportunities</b> to work in coding and modelling on both <b>regular</b> and <b>freelance</b> basis.</p>
+              <p className='leading-text'>Check out what've I built and made below !</p>
+            </div >
+          <div className='col-5'>
+            <img src={process.env.PUBLIC_URL+'/me.png'} className='myphoto rounded-circle' alt="chloe's icon"/>
+          </div>
 
-      </div>
+        </div>
        
       </div> 
     </div>
--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,10 +1,23 @@
 import React from 'react'
-import { Navbar, Nav, Container, Row,Col } from 'react-bootstrap';
+import { Stack, Row, Col, ProgressBar, Container } from 'react-bootstrap';
+import { ReactComponent as LeftSvg } from './littlegirl.svg';
 
 export default function Skills() {
   return (
-    <Row id="skills" style={{ height: '100vh' }}>
-        <Col style={{ backgroundColor: 'blue' }}>Skills</Col>
+    <Row id="skills" style={{ height: '100vh' , flexDirection:"column" }}>
+      <Col style={{ backgroundColor: 'rgb(50, 50, 57)', justifyContent:"center"}}>
+        <Container style={{ width:"60%", height:"100%", borderRadius:10, padding:5, display:"flex", flexDirection:"row", alignItems:"center"}}>
+          <Col xs={12} md={4}>
+            <LeftSvg />
+          </Col>
+          <Col xs={12} md={8}>
+            <h2>Skills</h2>
+            <ProgressBar now={80} variant="info" style={{marginBottom:20}}/>
+            <ProgressBar now={60} variant="info" style={{marginBottom:20}}/>
+            <ProgressBar now={40} variant="info" />
+          </Col>
+        </Container>
+        </Col>
     </Row>
   )
 }
