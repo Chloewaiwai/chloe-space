e49509c056da8d5bbb39d0ca5c4bb463e72c5685
Chloewaiwai <waiyip814@gmail.com>
2023-05-18 22:05:52 +0800

    reveal animation of contact

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,9 +1,25 @@
+import React, { useRef, useEffect } from 'react';
+import ScrollReveal from 'scrollreveal';
 import './Contact.css';
 import ContactForm from '../props/ContactForm';
 import MyInfo from '../props/MyInfo';
 import { Container, Row, Col, Image} from 'react-bootstrap';
 
 export default function Contact() {
+
+  const formRef = useRef(null);
+
+  useEffect(() => {
+    const sr = ScrollReveal({
+      duration: 2000,
+      distance: '30px',
+      easing: 'ease-out',
+      reset: true,
+    });
+
+    sr.reveal(formRef.current, { origin: 'left', delay: 200 });
+  }, []);
+
   return (
     <Row id="contact" style={{ minHeight: '100vh' , flexDirection:"column"}}>
         <Col style={{ backgroundColor: 'rgb(50, 50, 57)', justifyContent:"center", height: '100%', display: 'flex', alignItems: 'center'}} 
@@ -16,7 +32,7 @@ export default function Contact() {
                 <MyInfo />
               </Col>
               <Col>
-                <Container className='contact-form'>
+                <Container className='contact-form' ref={formRef}>
                   <Row>
                     <Col md={8} ><ContactForm /></Col>
                     <Col></Col>
--- src/props/MyInfo.js
+++ src/props/MyInfo.js
@@ -1,4 +1,5 @@
-import React from 'react'
+import React, { useRef, useEffect } from 'react';
+import ScrollReveal from 'scrollreveal';
 import { Row, Col} from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faEnvelope} from '@fortawesome/free-solid-svg-icons';
@@ -8,10 +9,21 @@ import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 
 export default function MyInfo() {
 
+    const infoRef = useRef(null);
 
+    useEffect(() => {
+      const sr = ScrollReveal({
+        duration: 2000,
+        distance: '30px',
+        easing: 'ease-out',
+        reset: true,
+      });
+  
+      sr.reveal(infoRef.current, { origin: 'right'});
+    }, []);
 
   return (
-    <div className='my-info'>
+    <div className='my-info' ref={infoRef}>
         <div style={{display:"flex", flexDirection:"column", height:"100%", justifyContent:"space-around"}}>
             <Row style={{ alignItems:"center"}}>
             <Col xs={2} style={{display:"flex", justifyContent:"center"}}><FontAwesomeIcon icon={faEnvelope} size="xl" style={{color: "#323239",}} />
@@ -28,9 +40,7 @@ export default function MyInfo() {
                 </Col>
                 <Col style={{color: "#323239"}}>Yip Chloe</Col>
             </Row>
-
         </div>
-        
     </div>
   )
 }
